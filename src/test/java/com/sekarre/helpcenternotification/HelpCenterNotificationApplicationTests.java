0d721d33a34051807ed93d7477a97b00025326d4
Sekarre <zimny2121@gmail.com>
2022-08-20 19:58:57 +0200

    initial commit

---
+++ src/test/java/com/sekarre/helpcenternotification/HelpCenterNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sekarre.helpcenternotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpCenterNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
